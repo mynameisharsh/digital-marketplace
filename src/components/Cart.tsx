e612bde3b91be45593554f75875753cb0ad37db2
mynameisharsh <59533249+mynameisharsh@users.noreply.github.com>
2023-12-03 09:45:42 +0000

    Improved cart section

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 import { ShoppingCartIcon } from "lucide-react";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { formatPrice } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import Image from "next/image";
 
 const Cart = () => {
+
+  const itemCount = 0;
+  const fee = 1;
+
   return (
         <Sheet>
           <SheetTrigger className="group -m-2 flex items-center p-2"> <ShoppingCartIcon
@@ -17,6 +26,61 @@ const Cart = () => {
             <SheetHeader className="space-y-2.5 pr-6">
               <SheetTitle>Cart (0)</SheetTitle>
             </SheetHeader>
+
+            {itemCount > 0 ? (
+              <>
+              <div className="flex w-full flex-col pr-6">
+                Cart Items
+              </div>
+              <div className="space-y-4 pr-6">
+                <Separator />
+                <div className="space-y-1.5 pr-6">
+                  <div className="flex">
+                    <span className="flex-1">
+                      Shipping
+                    </span>
+                    <span>Free</span>
+                  </div>
+                  <div className="flex">
+                    <span className="flex-1">
+                      Transaction Fee
+                    </span>
+                    <span>{formatPrice(fee)}</span>
+                  </div>
+                  <div className="flex">
+                    <span className="flex-1">
+                      Total
+                    </span>
+                    <span>{formatPrice(fee)}</span>
+                  </div>
+                </div>
+              </div>
+
+              <SheetFooter>
+                <SheetTrigger asChild>
+                  <Link href="/cart" className={buttonVariants({
+                    className: "w-full"
+                  })}>Continue to Checkout</Link>
+                </SheetTrigger>
+              </SheetFooter>
+              </>
+            ): <div className="flex h-full flex-col items-center justify-center space-y-1">
+              <div aria-hidden="true" className="relative mb-4 h-60 w-60 text-muted-foreground">
+                <Image src="/hippo-empty-cart.png" fill alt="Empty shopping cart" />
+              </div>
+              <div className="text0xl font-semibold">
+                Your cart is empty
+              </div>
+              <SheetTrigger asChild>
+                <Link href="/products" className={buttonVariants({
+                  variant: "link",
+                  size: "sm",
+                  className: "text-sm text-muted-forground"
+                })}>Add items to your cart</Link>
+              </SheetTrigger>
+
+            </div> }
+
            </SheetContent>
         </Sheet>
     )
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function formatPrice(price: number | string, options: {
+  currency?: "USD" | "EUR" | "GBP" | "INR"
+  notation?: Intl.NumberFormatOptions['notation']
+} = {}) {
+  const { currency = "USD", notation = 'compact' } = options;
+
+  const numericValue: number = typeof price === "string" ? parseFloat(price) : price;
+
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+    notation,
+    maximumFractionDigits: 2
+  }).format(numericValue)
+}
