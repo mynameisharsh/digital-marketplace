192b9e54c753b4d5a0573c46a7fdec57bdab8405
mynameisharsh <59533249+mynameisharsh@users.noreply.github.com>
2023-12-17 08:19:39 +0000

    Initial commit for payload CMS config

---
+++ src/payload.config.ts
@@ -0,0 +1,29 @@
+import { slateEditor } from "@payloadcms/richtext-slate";
+import { buildConfig } from "payload/config";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+import path from "path";
+
+export default buildConfig({
+    serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
+    collections: [],
+    routes: {
+        admin: '/sell'
+    },
+    admin: {
+        bundler:  webpackBundler(),
+        meta: {
+            titleSuffix: " - DigitalMarketplace"
+        }
+    },
+    rateLimit: {
+        max: 2000
+    },
+    editor: slateEditor({}),
+    db: mongooseAdapter({
+        url: process.env.MONGODB_URL!
+    }),
+    typescript: {
+        outputFile: path.resolve(__dirname, "payload-types.ts")
+    }
+})
